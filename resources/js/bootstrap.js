a5036651739b779161694e8ba5cf9a175d424d9a
reza <bghavipor@gmail.com>
2024-03-24 00:15:23 +0330

    add and config reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+window.axios = axios;
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
@@ -13,3 +15,17 @@ window.Echo = new Echo({
     enabledTransports: ['ws', 'wss'],
 });
 
+
+
+
+
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+
+Broadcast::channel('MessageSend.{receiverId}', function ($user,   $receiverId) {
+    return $user->id == $receiverId;
+});
+
+Broadcast::channel('chat-room.{chatRoomId}', function ($user,   $chatRoomId) {
+   return true;
+});
